97154da3c527e03a461063fb702e613ab0f07ced
phoenix-gi <ilyas_gi@mail.ru>
2020-09-05 14:52:45 +0300

    Create: index.php file

---
+++ src/php/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+            Single page application
+        </title>
+    </head>
+    <body>      
+        <?php 
+            echo "<h1>INDEX.PHP</h1>";
+        ?>
+    </body>
+</html>
